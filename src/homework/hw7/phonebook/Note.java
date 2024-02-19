1f93cd04661f4838317cce448a7a5b3d476d65df
vstarchevskyi <v.starchevskyi@zakaz.global>
2024-02-20 00:38:25 +0200

    add hw7 Phonebook

---
+++ src/homework/hw7/phonebook/Note.java
@@ -0,0 +1,26 @@
+package homework.hw7.phonebook;
+
+public class Note {
+
+    private String name;
+    private String phoneNumber;
+
+    public Note(String name, String phoneNumber) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    @Override
+    public String toString() {
+        return " name = '" + name + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'';
+    }
+}
