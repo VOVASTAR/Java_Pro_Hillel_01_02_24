7a5a82be130d19280c38d7638174b561705245bf
v.starchevskyi <v.starchevskyi@zakaz.global>
2024-05-30 23:25:22 +0300

    hw21_annotation

---
+++ src/homework/hw21_annotation/Test.java
@@ -0,0 +1,15 @@
+package homework.hw21_annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface Test {
+
+    String type() default "method test";
+
+    int priority() default 10;
+}
